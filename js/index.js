274ff8ef45e5b5748f3080e4af4c23e0006c8a36
haini000 <haini000@naver.com>
2025-11-24 17:33:57 +0900

    feat: back to top

--- js/index.js
+++ js/index.js
@@ -3,3 +3,25 @@
 2. 후기 슬라이드 (06-horizontal)
 3. about page (Q&A accodion)
 */
+
+const goTop = document.querySelector('#btt');
+
+window.addEventListener('scroll', () => {
+    let scrAmt = window.scrollY;
+
+    if (scrAmt >= 300) {
+        goTop.classList.add('active');
+    } else {
+        goTop.classList.remove('active');
+    }
+
+    goTop.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        window.scrollTo( {
+            left: 0,
+            top: 0,
+            behavior: 'smooth'
+        })
+    })
+})
